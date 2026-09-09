25ebc41315bcc378218622bcd239caad914597b1
ROHAN-8504 <203j1a4207@raguinstech.com>
2026-09-09 13:01:39 +0530

    examples

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-function sum(){
-  let a=  document.getElementById('fv').value
-  let b=  document.getElementById('sv').value
-  let result=parseInt(a)+parseInt(b)
-  document.getElementById('box').innerText=result
-  document.body.style.backgroundColor="black"
-  document.body.style.color="white"
+// function sum(){
+//   let a=  document.getElementById('fv').value
+//   let b=  document.getElementById('sv').value
+//   let result=parseInt(a)+parseInt(b)
+//   document.getElementById('box').innerText=result
+//   document.body.style.backgroundColor="black"
+//   document.body.style.color="white"
 
-}
+// }
+// let sum=()=>{
+
+// }
+
+
+
+// products.map((n)=>{})
+
+
+
+
+
+
+
+
+// function sayhello(name){
+//     //
+// }
+
+// //arrow functions
+// let sayhello=()=>{
+
+// }
+
+// function(){
+
+// }
+// ()=>{}
+
+let name="rohan"
+console.log(`my name is ${name}`)
