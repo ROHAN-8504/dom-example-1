84da82ac0d1190de57a5ff6b07cf399f43921431
ROHAN-8504 <203j1a4207@raguinstech.com>
2026-09-09 09:30:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+function ct(){
+    document.getElementById('head').innerText="hello js"
+    return;
+}
+function cc(){
+    document.getElementById('head').style.color="red"
+    return;
+}
+function dm(){
+    document.body.style.backgroundColor="black"
+    document.body.style.color="white"
+    return;
+}
+function resize(){
+    document.getElementById('image').style.height="50px"
+    document.getElementById('image').style.width="50px"
+    return;
+}
